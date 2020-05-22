29542307a807e98b483f37844e101ba94ef3ae3a
edkrueger <edkrueger@gmail.com>
2020-05-22 10:59:14 -0500

    Execute a function on a worker.

---
+++ functions.py
@@ -0,0 +1,4 @@
+import time
+def some_long_function(some_input):
+     time.sleep(10)
+     return some_input
--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-import redis
 import os
+import time
 
-r = redis.Redis(
+import redis
+from functions import some_long_function
+from rq import Queue
+
+redis_conn = redis.Redis(
      host=os.getenv("REDIS_HOST", "127.0.0.1"),
      port=os.getenv("REDIS_PORT", "6379"),
      password=os.getenv("REDIS_PASSWORD", "")
 )
 
+redis_queue = Queue(connection=redis_conn)
+
+job = redis_queue.enqueue(some_long_function, "Hello World!")
+
+while True:
+     if job.result:
+          print(job.result)
+          break
