ea7750c03c5a6508a1ac03b8b485153d4cfeb073
edkrueger <edkrueger@gmail.com>
2020-05-27 11:42:21 -0500

    Add enqueue route.

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import os
 import time
 
+from flask import Flask, jsonify, request
 from functions import some_long_function
-from redis_resc import redis_queue
+from redis_resc import redis_queue, redis_conn
+from rq.job import Job
 
-while True:
-     redis_queue.enqueue(some_long_function, f"This message was queued it {time.time()}")
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+     return "Running!"
+
+@app.route("/enqueue")
+def enqueue():
+     job = redis_queue.enqueue(some_long_function, f"This message was queued it {time.time()}")
+     return jsonify({
+          "job_id": job.id
+     })
+
+if  __name__ == "__main__":
+     app.run(debug=True)
