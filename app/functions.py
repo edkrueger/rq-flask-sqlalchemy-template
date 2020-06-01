6dc4d1babcd5a511d714e5c673d146808bd3ab9d
edkrueger <edkrueger@gmail.com>
2020-06-01 10:51:40 -0500

    Have the function write to database.

--- app/functions.py
+++ app/functions.py
@@ -2,11 +2,38 @@ import time
 
 from rq import get_current_job
 
+from . import models
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 def some_long_function(some_input):
         job = get_current_job()
         time.sleep(10)
+        # datetime.datetime
+        print(type(job.enqueued_at))
+        print(type(job.started_at))
+
+        db = SessionLocal()
+
+        result = models.Result(
+            job_id=job.id,
+            job_enqueued_at=job.enqueued_at,
+            job_started_at=job.started_at,
+            input=some_input,
+            result=some_input
+        )
+
+        db.add(result)
+        db.commit()
+        db.close()
+
+
+
         return {
             "job_id": job.id,
+            "job_enqueued_at": job.enqueued_at.isoformat(),
+            "job_started_at": job.started_at.isoformat(),
+            "input": some_input,
             "result": some_input
         }
