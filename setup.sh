e8b80cafbf2c75bb9ef36eba69eba5eefc022f2e
Edward Krueger <edkrueger@gmail.com>
2020-06-11 12:22:30 -0500

    Remove redundant/problematic command. Fixes #7.

--- setup.sh
+++ setup.sh
@@ -4,5 +4,4 @@ sudo sh get-docker.sh
 sudo curl -L https://github.com/docker/compose/releases/download/1.26.0/docker-compose-Linux-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-sudo groupadd docker
 sudo usermod -aG docker $USER
